39a91c3672a0f05c2072f61922822145cb41fdc4
Alex Jara <alex.jara@protonmail.com>
2025-02-10 11:51:42 -0500

    chore: temporarily disable audio auth for testing

--- app/Http/Middleware/AudioAuthenticate.php
+++ app/Http/Middleware/AudioAuthenticate.php
@@ -4,13 +4,12 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class AudioAuthenticate
 {
     public function handle(Request $request, Closure $next) // @phpcs:ignore
     {
-        abort_unless($request->user()?->tokenCan('audio'), Response::HTTP_UNAUTHORIZED);
+        // abort_unless($request->user()?->tokenCan('audio'), Response::HTTP_UNAUTHORIZED);
 
         return $next($request);
     }
